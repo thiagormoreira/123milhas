eadcb161d3d45dc99c817d0c514ceab8d9f2139b
Thiago Moreira <loganguns@gmail.com>
2021-07-10 23:49:06 -0300

    Created PHPUnit tests Edited index template GitHub CI config

--- tests/Feature/IndexTest.php
+++ tests/Feature/IndexTest.php
@@ -5,14 +5,14 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class IndexTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_index()
     {
         $response = $this->get('/');
 
